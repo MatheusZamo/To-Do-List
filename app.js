d64c94144e8a8ae7f5a6bc4d52b8bf639fce523e
Matheus Henrique <19matheushenrique03@gmail.com>
2024-02-29 22:28:02 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const formAddTodo = document.querySelector('.form-add-todo')
 const todosContainer = document.querySelector('.todos-container')
+const formSearch = document.querySelector('.form-search input')
 
 formAddTodo.addEventListener('submit',event => {
     event.preventDefault()
@@ -15,4 +16,27 @@ formAddTodo.addEventListener('submit',event => {
     }
     
       event.target.reset()
+})
+
+todosContainer.addEventListener('click',event => {
+    const clickTarget = event.target
+    if(Array.from(clickTarget.classList).includes('delete')){
+        clickTarget.parentElement.remove()
+    }
+})
+
+formSearch.addEventListener('input',event => {
+  const valueInput = event.target.value.trim().toLowerCase()
+  Array.from(todosContainer.children)
+    .filter(todo => !todo.textContent.toLowerCase().includes(valueInput))
+    .forEach(todo => {
+      todo.classList.remove('d-flex')
+      todo.classList.add('hidden')
+    })
+    Array.from(todosContainer.children)
+      .filter(todo => todo.textContent.toLowerCase().includes(valueInput))
+      .forEach(todo => {
+      todo.classList.remove('hidden')
+      todo.classList.add('d-flex')
+    })
 })
